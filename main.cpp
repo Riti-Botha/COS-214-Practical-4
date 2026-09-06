36071e80a2469f641e7b6a63939c7d09ae7e4ad5
Riti <151141333+Riti-Botha@users.noreply.github.com>
2026-09-06 22:41:15 +0200

    Implement emergency management system with 3 levels for composite

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "Composite.h"
+#include "Iterator.h"
+#include <iostream>
+
+int main() {
+    EmergencyTask* EmergencyMngr = new IncidentGroup("City Emergencies");
+
+    //level 1 composites in root component
+    EmergencyTask* City1 = new IncidentGroup("City 1 emergency");
+    EmergencyTask* City2 = new IncidentGroup("City 2 emergency");
+
+    // level 2 composites in each city
+    EmergencyTask* powerPlantFire = new IncidentGroup("Power plant fire emergency");
+    EmergencyTask* highwayCrash = new IncidentGroup("Highway crash emergency");
+
+    // level 3 leaves
+    EmergencyTask* fireTruck1 = new dispatchAmbulance("Truck 1 to power plant");
+    EmergencyTask* fireTruck2 = new dispatchAmbulance("Truck 2 to power plant");
+    EmergencyTask* ambulance1 = new dispatchAmbulance("Ambulance 1 to highway");
+    EmergencyTask* ambulance2 = new dispatchAmbulance("Ambulance 2 to highway");
+
+
+    powerPlantFire->add(fireTruck1);
+    powerPlantFire->add(fireTruck2);
+    highwayCrash->add(ambulance1);
+    City1->add(ambulance2);
+
+    City1->add(powerPlantFire);
+    City2->add(highwayCrash);
+
+    EmergencyMngr->add(City1);
+    EmergencyMngr->add(City2);
+
+    //test composite
+    EmergencyMngr->sendOut(); // recursive traversal
+
+    delete EmergencyMngr;
+
+    return 0;
+}
